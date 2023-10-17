0b28fb64aba6c2f4c155cb95c35419d1608c67cd
Dilara Cakmak <dilaracakmak2001@gmail.com>
2023-10-17 14:09:00 +0300

    left_to_right

--- extension.js
+++ extension.js
@@ -3,6 +3,8 @@ const terminalImage = require('terminal-image');
 const fs = require('fs');
 const path = require('path');
 
+let gifInterval;
+
 async function displayGif(gifPath) {
 
 	const gif = await terminalImage.gifFile(gifPath, {width: '10%', height: '10%'});
@@ -12,72 +14,74 @@ async function displayGif(gifPath) {
 	const terminalWidth = vscode.window.activeTerminal.dimensions.columns;
 	const terminalHeight = vscode.window.activeTerminal.dimensions.rows;
   
-	// Initial position at the bottom right corner
-	let xPos = terminalWidth - gifFrames[0].width;
-	const yPos = terminalHeight - gifFrames[0].height;
+	// Initial position at the bottom left corner
+    let xPos = 0;
+    const yPos = terminalHeight - gifFrames[0].height;
+	
+	// Move the cursor to the calculated position
+	const cursorTo = `\x1b[${yPos + 1};${xPos + 1}H`;
+	vscode.window.activeTerminal.sendText(cursorTo);
   
 	for (const frame of gifFrames) {
 		const imageData = frame.data;
 	
 		const imageString = await terminalImage.imageDataToString(imageData);
 	
-		// Move the cursor to the calculated position
-		const cursorTo = `\x1b[${yPos + 1};${xPos + 1}H`;
-		vscode.window.activeTerminal.sendText(cursorTo);
-	
 		// Display the GIF frame - run for each frame of the GIF
 		vscode.window.activeTerminal.renderFrame(imageString);
 	
-		// Move the dog horizontally to the left
-		xPos--;
+		// Move the GIF horizontally to the right
+		xPos++;
+		if (xPos + gifFrames[0].width > terminalWidth) {
+            xPos = 0;
+        }
 	
 		// Wait for a short period of time before displaying the next frame
 		await new Promise((resolve) => setTimeout(resolve, 100));
 	}
-  
-	// run when the animation playback is stopped
-	vscode.window.activeTerminal.renderFrame.done();
+
+	// Clear the line where the GIF was displayed
+    const clearLine = `\x1b[${yPos + 1};0H\x1b[K`;
+    vscode.window.activeTerminal.sendText(clearLine);
 }
+
+function moveGif(gifPath) {
+    gifInterval = setInterval(() => {
+        displayGif(gifPath);
+    }, 1800000);
+}
+
+function stopGif() {
+    clearInterval(gifInterval);
+}
+
+vscode.window.onDidCloseTerminal((terminal) => {
+    if (terminal === vscode.window.activeTerminal) {
+        stopGif();
+    }
+});
   
-  async function startGifLoop() {
+async function startGifLoop() {
 	const gifFolder = path.join(__dirname, 'img');
 	
 	while (true) {
 		const gifFiles = fs.readdirSync(gifFolder);
 		for (const gifFile of gifFiles) {
 			const gifPath = path.join(gifFolder, gifFile);
-			await displayGif(gifPath);
-		  }
-  
-		// Wait for 1 hour before displaying the next GIF
-		await new Promise((resolve) => setTimeout(resolve, 3600000));
+            moveGif(gifPath);
+            await new Promise((resolve) => setTimeout(resolve, 1800000)); // Wait for 30 min before displaying the next GIF
+            stopGif();
+		}
 	}
-  }
+}
 
 /**
  * @param {vscode.ExtensionContext} context
  */
-function activate(context) {
+function activate() {
 	startGifLoop();
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "dog-walk-terminal" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('dog-walk-terminal.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from dog-walk-terminal!');
-	});
-
-	context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
 function deactivate() {}
 
 module.exports = {
